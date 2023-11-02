a659c5e7cb4403a0ad63743229c07a2ce8d40326
Jorgen Swiderski <jorgen.swiderski@gmail.com>
2023-11-02 12:17:49 -0500

    Disable sentry in local environment

--- sentry.client.config.ts
+++ sentry.client.config.ts
@@ -3,28 +3,31 @@
 // https://docs.sentry.io/platforms/javascript/guides/nextjs/
 
 import * as Sentry from '@sentry/nextjs';
+import { CONFIG } from './src/models/config';
 
-Sentry.init({
-    dsn: 'https://90562fd20eaa3dc985c016fceb24bb8b@o4506137251676160.ingest.sentry.io/4506137256919040',
+if (!CONFIG.IS_DEV) {
+    Sentry.init({
+        dsn: 'https://90562fd20eaa3dc985c016fceb24bb8b@o4506137251676160.ingest.sentry.io/4506137256919040',
 
-    // Adjust this value in production, or use tracesSampler for greater control
-    tracesSampleRate: 1,
+        // Adjust this value in production, or use tracesSampler for greater control
+        tracesSampleRate: 1,
 
-    // Setting this option to true will print useful information to the console while you're setting up Sentry.
-    debug: false,
+        // Setting this option to true will print useful information to the console while you're setting up Sentry.
+        debug: false,
 
-    replaysOnErrorSampleRate: 1.0,
+        replaysOnErrorSampleRate: 1.0,
 
-    // This sets the sample rate to be 10%. You may want this to be 100% while
-    // in development and sample at a lower rate in production
-    replaysSessionSampleRate: 0.1,
+        // This sets the sample rate to be 10%. You may want this to be 100% while
+        // in development and sample at a lower rate in production
+        replaysSessionSampleRate: 0.1,
 
-    // You can remove this option if you're not planning to use the Sentry Session Replay feature:
-    integrations: [
-        new Sentry.Replay({
-            // Additional Replay configuration goes in here, for example:
-            maskAllText: true,
-            blockAllMedia: true,
-        }),
-    ],
-});
+        // You can remove this option if you're not planning to use the Sentry Session Replay feature:
+        integrations: [
+            new Sentry.Replay({
+                // Additional Replay configuration goes in here, for example:
+                maskAllText: true,
+                blockAllMedia: true,
+            }),
+        ],
+    });
+}
--- sentry.edge.config.ts
+++ sentry.edge.config.ts
@@ -4,13 +4,16 @@
 // https://docs.sentry.io/platforms/javascript/guides/nextjs/
 
 import * as Sentry from '@sentry/nextjs';
+import { CONFIG } from './src/models/config';
 
-Sentry.init({
-    dsn: 'https://90562fd20eaa3dc985c016fceb24bb8b@o4506137251676160.ingest.sentry.io/4506137256919040',
+if (!CONFIG.IS_DEV) {
+    Sentry.init({
+        dsn: 'https://90562fd20eaa3dc985c016fceb24bb8b@o4506137251676160.ingest.sentry.io/4506137256919040',
 
-    // Adjust this value in production, or use tracesSampler for greater control
-    tracesSampleRate: 1,
+        // Adjust this value in production, or use tracesSampler for greater control
+        tracesSampleRate: 1,
 
-    // Setting this option to true will print useful information to the console while you're setting up Sentry.
-    debug: false,
-});
+        // Setting this option to true will print useful information to the console while you're setting up Sentry.
+        debug: false,
+    });
+}
