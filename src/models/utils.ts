51c91c7dca857ae3c5645c9e337676bdc6e45533
Jorgen Swiderski <jorgen.swiderski@gmail.com>
2023-11-28 20:27:12 -0600

    Improve image preload strategy by direct embedding the image after preloading it instead of serving via a 302. Improve preloader to also load the assets of forced options and choices in the same manner that FeaturePicker does.

--- src/models/utils.ts
+++ src/models/utils.ts
@@ -1,9 +1,11 @@
 import { ICharacterOption } from '@jorgenswiderski/tomekeeper-shared/dist/types/character-feature-customization-option';
 import { SharedUtils } from '@jorgenswiderski/tomekeeper-shared/dist/models/utils';
 import assert from 'assert';
+import { IActionEffect } from '@jorgenswiderski/tomekeeper-shared/dist/types/grantable-effect';
 import { WeaveImages } from '../api/weave/weave-images';
 import { error } from './logger';
 import { CONFIG } from './config';
+import { characterDecisionInfo } from './character/character-states';
 
 type Difference = {
     path: string;
@@ -72,14 +74,59 @@ export class Utils extends SharedUtils {
             WeaveImages.preloadImage(image).catch(error);
         });
 
+        function reducer(option: ICharacterOption): ICharacterOption[] {
+            const allOptions = [option];
+
+            if (option?.choices) {
+                const flattened = option.choices
+                    .filter(
+                        (choice) =>
+                            choice.forcedOptions &&
+                            choice.forcedOptions.length >=
+                                choice.options.length,
+                    )
+                    .flatMap((choice) => choice.forcedOptions!)
+                    .flatMap(reducer);
+
+                allOptions.push(...flattened);
+            }
+
+            return allOptions;
+        }
+
+        const allOptions = options.flatMap(reducer);
+
         // Then the effect images
-        const fxImages = options
-            .flatMap((option) => [
-                ...(option.grants ? option.grants.map((fx) => fx.image) : []),
-            ])
+        const fxImages = allOptions
+            .flatMap((option) => option.grants ?? [])
+            .map((effect) => {
+                return (
+                    effect?.image ?? (effect as IActionEffect)?.action?.image
+                );
+            })
             .filter(Boolean) as string[]; // Filters out null or undefined values
 
         fxImages.forEach((image) => {
+            WeaveImages.preloadImage(image).catch(error);
+        });
+
+        // And the choice images
+        const choiceImages = allOptions
+            .filter((option) => option.choices)
+            .flatMap(
+                (option) =>
+                    option
+                        .choices!.map(
+                            (choice) =>
+                                characterDecisionInfo[choice.type]?.image?.(
+                                    option,
+                                    choice,
+                                ),
+                        )
+                        .filter(Boolean) as string[],
+            );
+
+        choiceImages.forEach((image) => {
             WeaveImages.preloadImage(image).catch(error);
         });
     }
